3b5e19dddb06dbe64972defb55d504983d592df8
Arda Mert Keleş <ardakelesmert@gmail.com>
2025-06-02 19:49:50 +0300

    improvements

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -1,5 +1,8 @@
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import '../styles/reservationPage/style.css'
+import littleImg1 from '../assets/img/reservationPage/littleImg1.jpg'
+import littleImg2 from '../assets/img/reservationPage/littleImg2.jpg'
+import littleImg3 from '../assets/img/reservationPage/littleImg3.jpg'
 
 export const ReservationPage = () => {
 
@@ -26,7 +29,48 @@ export const ReservationPage = () => {
             </div>
             <p className="article">Book a Private Room</p>
             <div className="private-room-container">
-
+                <div className="room-box">
+                    <img src={littleImg1} alt="" />
+                    <div className="info-section">
+                        <div className="room-details">
+                            <div className="room-name">Polo Room</div>
+                            <div className="room-info">This room offers an airy private dining space, with an outdoor patio to enjoy pre-dinner aperitifs. Concealed entry to the bar also gives discreet access to their wide range of whiskies and cocktails. A dining space where friendships are forged from across the table.</div>
+                        </div>
+                        <div className="details">
+                            <p>Capacity: Max 8 Guests</p>
+                            <p>Minimum Spend: $1500</p>
+                        </div>
+                    </div>
+                    <button className="reservation">Reserve</button>
+                </div>
+                <div className="room-box">
+                    <img src={littleImg2} alt="" />
+                    <div className="info-section">
+                        <div className="room-details">
+                            <div className="room-name">Wild Flower</div>
+                            <div className="room-info">Guests are seated around a marble-topped table featuring hand in-laid wildflowers.</div>
+                        </div>
+                        <div className="details">
+                            <p>Capacity: Max 6 Guests</p>
+                            <p>Minimum Spend: $1000</p>
+                        </div>
+                    </div>
+                    <button className="reservation">Reserve</button>
+                </div>
+                <div className="room-box">
+                    <img src={littleImg3} alt="" />
+                    <div className="info-section">
+                        <div className="room-details">
+                            <div className="room-name">Nilgiri</div>
+                            <div className="room-info">Featuring the enchanting Nilgiri Room,  the mesmerising wallpaper with flora and fauna, hand painted in India, provides a seamless panoramic view of the property's lush gardens.</div>
+                        </div>
+                        <div className="details">
+                            <p>Capacity: Max 24 Guests</p>
+                            <p>Minimum Spend: $5000</p>
+                        </div>
+                    </div>
+                    <button className="reservation">Reserve</button>
+                </div>
             </div>
             <p className="article">Indulge in our Bar!</p>
             <div className="indulge-bar-container">
