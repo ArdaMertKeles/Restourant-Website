7981fe8af7b05f708917f275bedc6b24cc1b89c7
Arda Mert Keleş <ardakelesmert@gmail.com>
2025-06-09 15:17:57 +0300

    Completation of Voucher Page

--- src/pages/VoucherPage.jsx
+++ src/pages/VoucherPage.jsx
@@ -10,75 +10,82 @@ export const VoucherPage = () => {
 
     return (
         <div className="voucher-page-wrapper">
-            <div className="left-side">
-                <img src={voucher} alt="" />
+            <h1>Inehsit e-Vouchers</h1>
+            <div className="container">
+                <div className="left-side">
+                    <div className="img-container">
+                        <img src={voucher} alt="voucher" />
+                    </div>
+                </div>
+                <div className="right-side">
+                    <p className='article'>e-Voucher</p>
+                    <p className='price'>${price}</p>
+                    <p className='terms'>You can't go wrong with an e-Voucher. Choose an amount and write a personalized message to make this e-Voucher your own. Terms and Conditions Apply.</p>
+                    <p className='denomination'>Denomination</p>
+                    <div className='prices'>
+                        <label onClick={() => setPrice(50)}>
+                            <input type="radio" name='price' defaultChecked='true' />
+                            <span>$50</span>
+                        </label>
+                        <label onClick={() => setPrice(100)}>
+                            <input type="radio" name='price' />
+                            <span>$100</span>
+                        </label>
+                        <label onClick={() => setPrice(150)}>
+                            <input type="radio" name='price' />
+                            <span>$150</span>
+                        </label>
+                        <label onClick={() => setPrice(200)}>
+                            <input type="radio" name='price' />
+                            <span>$200</span>
+                        </label>
+                        <label onClick={() => setPrice(300)}>
+                            <input type="radio" name='price' />
+                            <span>$300</span>
+                        </label>
+                        <label onClick={() => setPrice(500)}>
+                            <input type="radio" name='price' />
+                            <span>$500</span>
+                        </label>
+                    </div>
+                    <p className='quantity'>Quantity</p>
+                    <div className="amount-button">
+                        <button onClick={() => amount === 1 ? '' : setAmount(amount - 1)} disabled={amount === 1 ? true : false}>-</button>
+                        <p>{amount}</p>
+                        <button onClick={() => setAmount(amount + 1)}>+</button>
+                    </div>
+                    <p className='who'>Who's the e-Voucher for?</p>
+                    <div className='classifer'>
+                        <label onClick={() => setForMyself(false)}>
+                            <input type="radio" name='classifer' defaultChecked='true' />
+                            <div className='radio-label'>For someone else</div>
+                        </label>
+                        <label onClick={() => setForMyself(true)}>
+                            <input type="radio" name='classifer' />
+                            <div className='radio-label'>For myself</div>
+                        </label>
+                    </div>
+                    {!forMyself && <form className="someone-else-container">
+                        <label htmlFor="email">
+                            Recipent email*
+                            <input type="text" id='email' required='true' />
+                        </label>
+                        <label htmlFor="name">
+                            Recipent name
+                            <input type="text" id="name" />
+                        </label>
+                        <label htmlFor="message">
+                            Message
+                            <textarea name="message"></textarea>
+                        </label>
+                        <input type="submit" id='submit' />
+                    </form>}
+                    <label htmlFor="submit" className='buy-btn'>
+                        Buy Now
+                    </label>
+                </div>
             </div>
-            <div className="right-side">
-                <p className='article'>e-Voucher</p>
-                <p className='price'>${price}</p>
-                <p className='terms'>You can't go wrong with an e-Voucher. Choose an amount and write a personalized message to make this e-Voucher your own. Terms and Conditions Apply.</p>
-                <div className='prices'>
-                    <label onClick={() => setPrice(50)}>
-                        <input type="radio" name='price' defaultChecked='true' />
-                        <span>$50</span>
-                    </label>
-                    <label onClick={() => setPrice(100)}>
-                        <input type="radio" name='price' />
-                        <span>$100</span>
-                    </label>
-                    <label onClick={() => setPrice(150)}>
-                        <input type="radio" name='price' />
-                        <span>$150</span>
-                    </label>
-                    <label onClick={() => setPrice(200)}>
-                        <input type="radio" name='price' />
-                        <span>$200</span>
-                    </label>
-                    <label onClick={() => setPrice(300)}>
-                        <input type="radio" name='price' />
-                        <span>$300</span>
-                    </label>
-                    <label onClick={() => setPrice(500)}>
-                        <input type="radio" name='price' />
-                        <span>$500</span>
-                    </label>
-                </div>
-                <p>Quantity</p>
-                <div className="amount-button">
-                    <button onClick={() => amount === 1 ? '' : setAmount(amount - 1)} disabled={amount === 1 ? true : false}>-</button>
-                    <p>{amount}</p>
-                    <button onClick={() => setAmount(amount + 1)}>+</button>
-                </div>
-                <p>Who's the e-Voucher for?</p>
-                <div className='classifer'>
-                    <label onClick={() => setForMyself(false)}>
-                        <input type="radio" name='classifer' defaultChecked='true' />
-                        <div>For someone else</div>
-                    </label>
-                    <label onClick={() => setForMyself(true)}>
-                        <input type="radio" name='classifer' />
-                        <div>For myself</div>
-                    </label>
-                </div>
-                {!forMyself && <form className="someone-else-container">
-                    <label htmlFor="email">
-                        Recipent email*
-                    </label>
-                    <input type="text" id='email' required='true' />
-                    <label htmlFor="name">
-                        Recipent name
-                    </label>
-                    <input type="text" id="name" />
-                    <label htmlFor="message">
-                        Message
-                    </label>
-                    <input type="text" id='message' />
-                    <input type="submit" id='submit' />
-                </form>}
-                <label htmlFor="submit">
-                    Buy Now
-                </label>
-            </div>
+            <div className="voucher-footer">Tel: 0 555 555 55 55 | dine@inehsit.com.tr | ©2025 Inehsit. All Rights Reserved.</div>
         </div>
     )
 }
